d6e902fa5d572074e108ef48c44dc1f3bc495801
Steve Shipman <shipman@ncf.edu>
2019-07-02 15:01:06 -0400

    Updated code with interactivity and mostly generalized on numbers of dimensions

--- python-pattern-search.py
+++ python-pattern-search.py
@@ -48,12 +48,16 @@ version 2 - works on other functions and can implement bounds!
 short term to-do: generalize on number of dimensions
 longer term: build GUI
 
+version 3 - added interactivity and generalized the number of dimensions, mostly, though depends on user to make sure that optimization_function is consistent with number used
+next steps: time to start thinking about turning the interactive parts into the GUI!
+
 """
 
 import numpy as np
 import math
+import sys
 
-def optimization_function(point): # Currently fixed at 2-D
+def optimization_function(point): # Need to make sure that functions match with number of dimensions; not really an obvious way to do that, probably should just have that be a specification of each test function.
 # Spherical function
 	#x = point[0]
 	#y = point[1]
@@ -69,32 +73,31 @@ def optimization_function(point): # Currently fixed at 2-D
 #	y = point[1]
 #	z = (1.5-x+x*y)**2 + (2.25-x+x*y**2)**2 + (2.625-x+x*y**3)**2
 
-# Rastrigin function
-#	x = point[0]
-#	y = point[1]
-#	z = 20.0 + (x**2 - 10*math.cos(2*math.pi*x)) + (y**2 - 10*math.cos(2*math.pi*y))
+# Rastrigin function - generalized to multi-dimensions, yay!
+
+	z = 10*(len(point))
+	for i in range(len(point)):
+		x_i = point[i]
+		z += (x_i**2 - 10*math.cos(2*math.pi*x_i))
 
 # Easom function
-	x = point[0]
-	y = point[1]
-	z = (-1)*math.cos(x)*math.cos(y)*math.exp((-1)*((x-math.pi)**2 + (y-math.pi)**2))
+#	x = point[0]
+#	y = point[1]
+#	z = (-1)*math.cos(x)*math.cos(y)*math.exp((-1)*((x-math.pi)**2 + (y-math.pi)**2))
 
 	return z
 
-def bounds_check(point): # Assumes 2-D, though looks straightforward to generalize
-	if bounds == True:
-		if point[0] < x_min:
-			point[0] = x_min
-		if point[0] > x_max:
-			point[0] = x_max
-		if point[1] < y_min:
-			point[1] = y_min
-		if point[1] > y_max:
-			point[1] = y_max
+def bounds_check(point):
+
+	for i in range(len(point)):
+		if point[i] < min_bounds[i]:
+			point[i] = min_bounds[i]
+		if point[i] > max_bounds[i]:
+			point[i] = max_bounds[i]
 
 	return point
 
-def generate_steps(num_dimensions,step_size,point):
+def generate_steps(num_dimensions,step_size,point,bounds):
 	output_list = [point] # Include starting point as a thing to be evaluated
 
 	for i in range(num_dimensions):
@@ -132,32 +135,111 @@ def convergence_check(num_iterations,step_size):
 
 	return convergence_decision
 
+def integer_check(user_input): # Adapted from stackoverflow: https://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float
+	try:
+		int(user_input)
+		return True
+	except ValueError:
+		return False
+
+def YN_check(user_input):
+	if (user_input == "Y") or (user_input == "y") or (user_input == "N") or (user_input == "n"):
+		return True
+	else:
+		return False
+
+def float_check(user_input):
+	try:
+		float(user_input)
+		return True
+	except ValueError:
+		return False
 
 
 # Hardcoded parameters section
-num_dimensions = 2
-initial_x = 4.3 # Need to be determined based on number of dimensions instead of hardcoded
-initial_y = 4.7
 step_size = 1.0 # Assumes uniform step sizes for all dimensions, I *think* this is fine given pattern search grid employed here
 max_iterations = 1000
 min_step_size = 1E-6
-bounds = True # Says whether or not we want to check for bounds on the variables
-x_min = -100 # These should be dynamically created depending on number of dimensions and whether or not bounds is true
-x_max = 100
-y_min = -100
-y_max = 100
 #
+
+num_dimensions_user = raw_input("How many dimensions are in the search space? ") # Need raw_input, not input
+int_flag = integer_check(num_dimensions_user)
+
+while int_flag == False:
+	print("Your input wasn't an integer.\n")
+	num_dimensions_user = raw_input("How many dimensions are in the search space? ")
+	int_flag = integer_check(num_dimensions_user)
+
+num_dimensions = int(num_dimensions_user)
+
+initial_point = np.zeros(num_dimensions)
+
+for i in range(num_dimensions):
+	initial_point_user = raw_input("What is the initial coordinate for dimension %s? "%i)
+	valid_point_flag = float_check(initial_point_user)
+
+	while valid_point_flag == False:
+		print("Your input wasn't a valid number.\n")
+		initial_point_user = raw_input("What is the initial coordinate for dimension %s? "%i)
+		valid_point_flag = float_check(initial_point_user)
+
+	initial_point[i] = initial_point_user
+
+bounds_input = str.strip(raw_input("Do you want to put boundaries on the search range? (Y/N) "))
+bounds_input_flag = YN_check(bounds_input)
+
+while bounds_input_flag == False:
+	print("Your input wasn't either Y or N (or y or n).\n")
+	bounds_input = str.strip((raw_input("Do you want to put boundaries on the search range? (Y/N) ")))
+	bounds_input_flag = YN_check(bounds_input)
+
+if bounds_input == "Y" or bounds_input == "y":	# Loop above should guarantee that this is fine.
+	bounds = True
+	min_bounds = np.zeros(num_dimensions)
+	max_bounds = np.zeros(num_dimensions)
+else:
+	bounds = False
+
+
+if bounds == True:
+	for i in range(num_dimensions):
+		valid_range_flag = False
+
+		while (valid_range_flag == False):
+			bound_user_lower = raw_input("What is the lower bound for dimension %s? "%i)
+			valid_bound_flag = float_check(bound_user_lower)
+
+			while valid_bound_flag == False:
+				print("Your input wasn't a valid number.\n")
+				bound_user_lower = raw_input("What is the lower bound for dimension %s? "%i)
+				valid_bound_flag = float_check(bound_user_lower)
+
+			min_bounds[i] = float(bound_user_lower)
+
+			bound_user_upper = raw_input("What is the upper bound for dimension %s? "%i)
+			valid_bound_flag = float_check(bound_user_upper)
+
+			while valid_bound_flag == False:
+				print("Your input wasn't a valid number.\n")
+				bound_user_upper = raw_input("What is the upper bound for dimension %s? "%i)
+				valid_bound_flag = float_check(bound_user_upper)
+
+			max_bounds[i] = float(bound_user_upper)
+
+			if (min_bounds[i] >= max_bounds[i]):
+				print("Lower bound is greater than or equal to upper bound. Please re-enter both bounds.")
+			else:
+				valid_range_flag = True
 
 convergence_flag = 0
 num_iterations = 0
 
-initial_point = np.array([initial_x,initial_y])
 if bounds == True: # Move initial point within boundaries if it's not already in
 	initial_point = bounds_check(initial_point)
 
 while convergence_flag == 0:
 	num_iterations += 1
-	trial_points = generate_steps(num_dimensions,step_size,initial_point)
+	trial_points = generate_steps(num_dimensions,step_size,initial_point,bounds)
 	output_values = test_points(trial_points)
 	min_value = min(output_values) # Hard-coded optimizing for minimum...
 	min_pos = output_values.index(min_value)
@@ -175,4 +257,3 @@ while convergence_flag == 0:
 	convergence_flag = convergence_check(num_iterations,step_size)
 
 print "Finished!"
-
