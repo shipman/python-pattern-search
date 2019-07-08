05466003ee5376eed7d7ccf57b93008f2e8ffbd3
Steve Shipman <shipman@ncf.edu>
2019-07-08 13:11:52 -0400

    Got GUI completely up and running, integrated matplotlib to plot final results. Allowed for user choice of optimization function via a choice box. Yay!

--- pattern-search-gui.py
+++ pattern-search-gui.py
@@ -12,6 +12,15 @@ import numpy as np
 import math
 import sys
 
+import matplotlib
+matplotlib.use("Qt5Agg")
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from matplotlib.ticker import MaxNLocator
+
 class Ui_Dialog_First_Window(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
@@ -42,35 +51,43 @@ class Ui_Dialog_First_Window(object):
         self.min_ss_input.setToolTip("The search will consider itself converged if the step size is smaller than this value.")
         self.min_ss_input.setText("1E-6") # Default value
         self.gridLayout.addWidget(self.min_ss_input, 2, 1, 1, 2)
+        self.cb_label = QtWidgets.QLabel(Dialog)
+        self.cb_label.setObjectName("cb_label")
+        self.gridLayout.addWidget(self.cb_label, 3, 0, 1, 2)
+        self.cb = QtWidgets.QComboBox(Dialog)
+        self.cb.setObjectName("cb") # Do we have to add a function to deal with it if someone changes the value? Or will it just work?
+        self.cb.setToolTip("This is the function to be optimized.")
+        self.cb.addItems(["Spherical", "Ackley", "Beale", "Rastrigin", "Easom"])
+        self.gridLayout.addWidget(self.cb, 3, 2, 1, 1)
         self.num_dim_label = QtWidgets.QLabel(Dialog)
         self.num_dim_label.setObjectName("num_dim_label")
-        self.gridLayout.addWidget(self.num_dim_label, 3, 0, 1, 2)
+        self.gridLayout.addWidget(self.num_dim_label, 4, 0, 1, 2)
         self.num_dim_input = QtWidgets.QSpinBox(Dialog)
         self.num_dim_input.setObjectName("num_dim_input")
         self.num_dim_input.setToolTip("This is the number of dimensions in the search space.")
         self.num_dim_input.setMinimum(1) #Need to be searching along at least one dimension!
         self.num_dim_input.setValue(2) # Default value
-        self.gridLayout.addWidget(self.num_dim_input, 3, 2, 1, 1)
+        self.gridLayout.addWidget(self.num_dim_input, 4, 2, 1, 1)
         self.bound_label = QtWidgets.QLabel(Dialog)
         self.bound_label.setObjectName("bound_label")
-        self.gridLayout.addWidget(self.bound_label, 4, 0, 1, 2)
+        self.gridLayout.addWidget(self.bound_label, 5, 0, 1, 2)
         self.bound_cb = QtWidgets.QCheckBox(Dialog)
         self.bound_cb.setObjectName("bound_cb")
         self.bound_cb.setToolTip("This indicates whether boundaries should be applied to coordinates.")
-        self.gridLayout.addWidget(self.bound_cb, 4, 2, 1, 1)
+        self.gridLayout.addWidget(self.bound_cb, 5, 2, 1, 1)
         self.next_steps_button = QtWidgets.QPushButton(Dialog)
         self.next_steps_button.setObjectName("next_steps_button")
         self.next_steps_button.clicked.connect(self.proc_input)
-        self.gridLayout.addWidget(self.next_steps_button, 5, 0, 1, 1)
+        self.gridLayout.addWidget(self.next_steps_button, 6, 0, 1, 1)
         self.run_search_button = QtWidgets.QPushButton(Dialog)
         self.run_search_button.setObjectName("run_search_button")
         self.run_search_button.setEnabled(False)
         self.run_search_button.clicked.connect(self.run_search) # Change this to actually do the thing later
-        self.gridLayout.addWidget(self.run_search_button, 5, 1, 1, 2)
+        self.gridLayout.addWidget(self.run_search_button, 6, 1, 1, 2)
         self.exit_button = QtWidgets.QPushButton(Dialog)
         self.exit_button.setObjectName("exit_button")
         self.exit_button.clicked.connect(app.quit)
-        self.gridLayout.addWidget(self.exit_button, 6, 0, 1, 3)
+        self.gridLayout.addWidget(self.exit_button, 7, 0, 1, 3)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -81,6 +98,7 @@ class Ui_Dialog_First_Window(object):
         self.iter_label.setText(_translate("Dialog", "Maximum # of Iterations"))
         self.init_ss_label.setText(_translate("Dialog", "Initial Step Size"))
         self.min_ss_label.setText(_translate("Dialog", "Minimum Step Size"))
+        self.cb_label.setText(_translate("Dialog", "Function to Optimize"))
         self.num_dim_label.setText(_translate("Dialog", "# of Dimensions in Search Space"))
         self.bound_label.setText(_translate("Dialog", "Add Constraints on Coordinates?"))
         self.next_steps_button.setText(_translate("Dialog", "Next Steps"))
@@ -117,6 +135,8 @@ class Ui_Dialog_Input_Error_Window(QtWidgets.QDialog):
         self.exit_button.clicked.connect(Dialog2.close)
         self.gridLayout.addWidget(self.exit_button, 3, 0, 1, 1)
         self.exit_button.setText(_translate("Dialog2", "Exit"))
+        self.exit_button.setEnabled(False)
+
 
         QtCore.QMetaObject.connectSlotsByName(Dialog2)
         Dialog2.setWindowTitle(_translate("Dialog2", "Input Validation"))
@@ -125,6 +145,8 @@ class Ui_Dialog_Input_Error_Window(QtWidgets.QDialog):
     def update_value(self):
         updated_value = self.updated_input.text()
         self.error_message.setText("The updated value is %s. If you're happy with this, press Exit."%updated_value)
+        self.exit_button.setEnabled(True)
+
         return updated_value
 
 class Ui_Dialog_CoordInit_NoBounds(QtWidgets.QDialog):
@@ -132,6 +154,7 @@ class Ui_Dialog_CoordInit_NoBounds(QtWidgets.QDialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog3a.setObjectName("Dialog3a")
         Dialog3a.resize(100*num_dimensions,50)
+        Dialog3a.setWindowTitle(_translate("Dialog3a", "Coordinate Initialization"))
         self.gridLayout = QtWidgets.QGridLayout(Dialog3a)
         self.gridLayout.setObjectName("gridLayout")
         self.num_dimensions = num_dimensions
@@ -160,12 +183,15 @@ class Ui_Dialog_CoordInit_NoBounds(QtWidgets.QDialog):
         self.exit_button.clicked.connect(Dialog3a.close)
         self.gridLayout.addWidget(self.exit_button, 2, (num_dimensions/2), 1, (num_dimensions/2))
         self.exit_button.setText(_translate("Dialog3a", "Exit"))
+        self.exit_button.setEnabled(False)
 
 class Ui_Dialog_CoordInit_Bounds(QtWidgets.QDialog):
     def setupUi(self, Dialog3b, num_dimensions):
         _translate = QtCore.QCoreApplication.translate
         Dialog3b.setObjectName("Dialog3b")
         Dialog3b.resize(300,50*num_dimensions)
+        Dialog3b.setWindowTitle(_translate("Dialog3b", "Coordinate and Bounds Initialization"))
+
         self.gridLayout = QtWidgets.QGridLayout(Dialog3b)
         self.gridLayout.setObjectName("gridLayout")
         self.num_dimensions = num_dimensions
@@ -220,6 +246,7 @@ class Ui_Dialog_CoordInit_Bounds(QtWidgets.QDialog):
         self.exit_button.clicked.connect(Dialog3b.close)
         self.gridLayout.addWidget(self.exit_button, num_dimensions+1, 2, 1, 1) # fix positioning
         self.exit_button.setText(_translate("Dialog3b", "Exit"))
+        self.exit_button.setEnabled(False)
 
 class Ui_Dialog_Bounds_Error_Window(QtWidgets.QDialog):
     def setupUi(self, Dialog3c, Error_String):
@@ -263,6 +290,7 @@ class Ui_Dialog_Bounds_Error_Window(QtWidgets.QDialog):
         self.exit_button.clicked.connect(Dialog3c.close)
         self.gridLayout.addWidget(self.exit_button, 3, 1, 1, 1)
         self.exit_button.setText(_translate("Dialog3c", "Exit"))
+        self.exit_button.setEnabled(False)
 
         QtCore.QMetaObject.connectSlotsByName(Dialog3c)
         Dialog3c.setWindowTitle(_translate("Dialog3c", "Bounds Validation"))
@@ -296,35 +324,65 @@ class Ui_Dialog_Bounds_Error_Window(QtWidgets.QDialog):
         self.max_input.setText(updated_max)
 
         self.error_message.setText("The updated values for min and max are %s and %s. If you're happy with this, press Exit."%(updated_min,updated_max))
+        self.exit_button.setEnabled(True)
+
         return (updated_min,updated_max)
 
-def optimization_function(point): # Need to make sure that functions match with number of dimensions; not really an obvious way to do that, probably should just have that be a specification of each test function.
+class PlotCanvas(FigureCanvas): # Taken from http://littlecaptain.net/pyqt5-matplotlib.html
+    def __init__(self, xdata, ydata, distdata, parent=None, width=8, height=6, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        FigureCanvas.__init__(self,fig)
+        self.setWindowTitle("Plot of Optimization Results")
+
+        self.axes = fig.add_subplot(121)
+        self.axes.plot(xdata,ydata,'o')
+        self.axes.set_xlabel('Iteration #')
+        self.axes.set_ylabel('Value of Function')
+        self.axes.xaxis.set_major_locator(MaxNLocator(nbins=5,integer=True))
+
+        self.axes2 = fig.add_subplot(122)
+        self.axes2.plot(xdata,distdata,'o')
+        self.axes2.set_xlabel('Iteration #')
+        self.axes2.set_ylabel('Distance from Starting Point')
+        self.axes2.xaxis.set_major_locator(MaxNLocator(nbins=5,integer=True))
+
+        #fig.subplots_adjust(wspace=0.2)
+        fig.suptitle('Pattern Search Optimization Results')
+        fig.tight_layout(rect=[0, 0, 1, 0.95])
+
+        self.show()
+
+def optimization_function(point,choice): # Need to make sure that functions match with number of dimensions; not really an obvious way to do that, probably should just have that be a specification of each test function.
 # Spherical function
-    #x = point[0]
-    #y = point[1]
-    #z = x**2 + y**2
+    if choice == "Spherical":
+        x = point[0]
+        y = point[1]
+        z = x**2 + y**2
 
 # Ackley function
-    #x = point[0]
-    #y = point[1]
-    #z = -20.0*math.exp(-0.2*math.sqrt(0.5*(x**2 + y**2))) - math.exp(0.5*(math.cos(2*math.pi*x)+math.cos(2*math.pi*y))) + math.exp(1.0) + 20.0
+    if choice == "Ackley":
+        x = point[0]
+        y = point[1]
+        z = -20.0*math.exp(-0.2*math.sqrt(0.5*(x**2 + y**2))) - math.exp(0.5*(math.cos(2*math.pi*x)+math.cos(2*math.pi*y))) + math.exp(1.0) + 20.0
 
 # Beale function
-#   x = point[0]
-#   y = point[1]
-#   z = (1.5-x+x*y)**2 + (2.25-x+x*y**2)**2 + (2.625-x+x*y**3)**2
+    if choice == "Beale":
+        x = point[0]
+        y = point[1]
+        z = (1.5-x+x*y)**2 + (2.25-x+x*y**2)**2 + (2.625-x+x*y**3)**2
 
 # Rastrigin function - generalized to multi-dimensions, yay!
-
-    z = 10*(len(point))
-    for i in range(len(point)):
-        x_i = point[i]
-        z += (x_i**2 - 10*math.cos(2*math.pi*x_i))
+    if choice == "Rastrigin":
+        z = 10*(len(point))
+        for i in range(len(point)):
+            x_i = point[i]
+            z += (x_i**2 - 10*math.cos(2*math.pi*x_i))
 
 # Easom function
-#   x = point[0]
-#   y = point[1]
-#   z = (-1)*math.cos(x)*math.cos(y)*math.exp((-1)*((x-math.pi)**2 + (y-math.pi)**2))
+    if choice == "Easom":
+        x = point[0]
+        y = point[1]
+        z = (-1)*math.cos(x)*math.cos(y)*math.exp((-1)*((x-math.pi)**2 + (y-math.pi)**2))
 
     return z
 
@@ -356,11 +414,11 @@ def generate_steps(num_dimensions,step_size,point,min_bounds=[],max_bounds=[]):
 
     return output_list
 
-def test_points(point_list):
+def test_points(point_list,choice):
     output_value_list = []
 
     for point in point_list:
-        temp_output = optimization_function(point)
+        temp_output = optimization_function(point,choice)
         output_value_list.append(temp_output)
 
     return output_value_list
@@ -471,8 +529,10 @@ def report_init_coords(data_object):
             valid_bound_flag = True
 
     if data_object.bounds_flag == True:
+        data_object.exit_button.setEnabled(True)
         return init_coords,min_coords,max_coords
     else:
+        data_object.exit_button.setEnabled(True)
         return init_coords
 
 def process_input(data_object):
@@ -553,22 +613,13 @@ def process_input(data_object):
             temp_coord = float(ui.coords[i]["coord_value"].text())
             init_coords.append(temp_coord)
 
-    #print "Number of iterations: %s"%num_iterations
-    #print "Initial step size: %s"%init_ss
-    #print "Convergence step size: %s"%conv_ss
-    #print "Number of dimensions: %s"%num_dimensions
-    #print "Initial coordinates: %s"%init_coords
     data_object.init_coords = init_coords
 
     if need_bounds:
-        #print "Minimum boundaries: %s"%coord_bounds_min
         data_object.coord_bounds_min = coord_bounds_min
-        #print "Maximum boundaries: %s\n"%coord_bounds_max
         data_object.coord_bounds_max = coord_bounds_max
 
     data_object.run_search_button.setEnabled(True)
-
-    # At this point we then need to ungray the "Run Simulations" button and then actually do the thing!
 
 
 def pattern_search(data_object):
@@ -583,11 +634,13 @@ def pattern_search(data_object):
     num_dimensions = data_object.num_dim_input.value()
 
     init_coords = data_object.init_coords
+    choice = data_object.cb.currentText()
 
     if need_bounds:
         coord_bounds_min = data_object.coord_bounds_min
         coord_bounds_max = data_object.coord_bounds_max
 
+    print "Choice of optimization function: %s"%choice
     print "Number of iterations: %s"%max_iterations
     print "Initial step size: %s"%step_size
     print "Convergence step size: %s"%conv_ss
@@ -626,6 +679,11 @@ def pattern_search(data_object):
 
     print "Initial coordinates: %s\n"%init_coord_string
 
+    x_data = []
+    y_data = []
+    dist_data = []
+    orig_point = initial_point.copy()
+
     while convergence_flag == False:
         num_iterations += 1
 
@@ -634,12 +692,14 @@ def pattern_search(data_object):
         else:
             trial_points = generate_steps(num_dimensions,step_size,initial_point)
 
-        output_values = test_points(trial_points)
+        output_values = test_points(trial_points,choice)
         min_value = min(output_values) # Hard-coded optimizing for minimum...
         min_pos = output_values.index(min_value)
         best_point = trial_points[min_pos]
 
         print "Iteration #: %s, Best point: %s, Best value: %s, Step size: %s"%(num_iterations,best_point,min_value,step_size)
+        x_data.append(num_iterations)
+        y_data.append(min_value)
 
         if min_pos == 0:
             step_size = step_size*0.5
@@ -648,9 +708,23 @@ def pattern_search(data_object):
             initial_point = best_point
             step_size = step_size*2
 
+        temp_dist = 0
+
+        for i in range(num_dimensions):
+            temp_dist += (best_point[i] - orig_point[i])**2
+
+        temp_dist = math.sqrt(temp_dist)
+        dist_data.append(temp_dist)
+
         convergence_flag = convergence_check(num_iterations,max_iterations,step_size,conv_ss)
 
     print "Finished!\n"
+
+    m = PlotCanvas(x_data,y_data,dist_data,width=5,height=4,dpi=100)
+
+
+
+    data_object.run_search_button.setEnabled(False) # Grey out button so user has to set up a new run (maybe remove if this gets annoying?)
 
 
 if __name__ == "__main__":
@@ -659,9 +733,5 @@ if __name__ == "__main__":
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog_First_Window()
     ui.setupUi(Dialog)
-    #ui = Ui_Dialog_Input_Error_Window()
-    #test_string = "This is a test"
-    #ui.setupUi(Dialog, test_string)
     Dialog.show()
     sys.exit(app.exec_())
-
